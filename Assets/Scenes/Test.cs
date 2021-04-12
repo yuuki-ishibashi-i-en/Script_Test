2493173112a0d1210e39bf10315ee7a31804f652
yuuki-ishibashi-i-en <y-ishibashi@i-enter.co.jp>
2021-04-12 14:43:23 +0900

    Update Test.cs
    発展課題完了

--- Assets/Scenes/Test.cs
+++ Assets/Scenes/Test.cs
@@ -2,13 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Boss
+{
+    public int MP = 53;
+
+    public void Magic()
+    {
+        if (this.MP >= 5)
+        {
+            this.MP -= 5;
+            Debug.Log("魔法攻撃をした。残りMPは" + this.MP);
+        }
+
+        else if (this.MP < 5)
+        {
+            Debug.Log("MPが足りない!");
+        }
+    }
+
+}
+
 public class Test : MonoBehaviour
 {
+    Boss boss=new Boss();
+
 
     // Start is called before the first frame update
     void Start()
     {
-        int[] array = {1, 2, 3, 4, 5, };
+        int[] array = { 1, 2, 3, 4, 5, };
 
         for (int i = 0; i < array.Length; i++)
         {
@@ -21,6 +43,13 @@ public class Test : MonoBehaviour
             {
                 Debug.Log(array[i]);
             }
+        }
+
+        Debug.Log("ここから発展課題");
+
+        for (int i = 0; i <= 11; i++)
+        {
+            boss.Magic();
         }
     }
 }
